0983267bee38bbe56bbfdc18a4f71a998addd384
alexdarahakupets <alexdarahakupets@gmail.com>
2026-07-07 13:36:52 +0200

    thumbnail stored in db in base64

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,38 +4,6 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-import { getInMemoryURL } from "./assets";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -74,18 +42,17 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new BadRequestError('Missing Content-Type for thumbnail');
   }
 
-  const thumbnailImageData = await thumbnailFile.arrayBuffer();
-  if (!thumbnailImageData) {
+  const thumbnailImageArrayBuffer = await thumbnailFile.arrayBuffer();
+  if (!thumbnailImageArrayBuffer) {
     throw new Error('Error reading file data');
   }
+  const thumbnailImageBufferBase64 = Buffer.from(thumbnailImageArrayBuffer).toString('base64');
+  if (!thumbnailImageBufferBase64) {
+    throw new Error("Couldn't convert image to base64 buffer");
+  }
+  const thumbnailDataUrl = `data:${thumbnailMediaType};base64,${thumbnailImageBufferBase64}`
 
-  videoThumbnails.set(videoId, {
-    data: thumbnailImageData,
-    mediaType: thumbnailMediaType
-  } satisfies Thumbnail);
-
-  const thumbnailUrl = getInMemoryURL(cfg, videoId);
-  updateVideo(cfg.db, {...dbVideo, thumbnailURL: thumbnailUrl})
+  updateVideo(cfg.db, {...dbVideo, thumbnailURL: thumbnailDataUrl, })
 
   return respondWithJSON(200, null);
 }
